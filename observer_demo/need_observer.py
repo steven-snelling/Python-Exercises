845c3971e305150d6b2338df597ab956ee93aa05
Steve <stevensnelling@hotmail.com>
2017-11-01 20:47:23 +1300

    m

--- observer_demo/need_observer.py
+++ observer_demo/need_observer.py
@@ -1,22 +1,52 @@
-class DivObserver(object):
+from abc import ABCMeta, abstractmethod
+
+
+class Observer(metaclass=ABCMeta):
     def __init__(self, value):
         self.value = value
+        self.subject = None
+
+    @abstractmethod
+    def update(self, val):
+        pass
+
+    def set_subject(self, subject):
+        self.subject = subject
+
+
+class DivObserver(Observer):
 
     def update(self, val):
         print("{0} div {1} = {2}".format(
             val, self.value, val / self.value))
 
 
-class ModObserver(object):
-    def __init__(self, value):
-        self.value = value
+class ModObserver(Observer):
 
     def update(self, val):
         print("{0} mod {1} = {2}".format(
             val, self.value, val % self.value))
 
 
-class Subject(object):
+class AbstractSubject(metaclass=ABCMeta):
+
+    def __init__(self):
+        self.__observers = []
+
+    def attach(self, obs):
+        self.__observers.append(obs)
+
+    def detach(self, obs):
+        self.__observers.remove(obs)
+
+    def notify(self):
+        for o in self.__observers:
+            o.update()
+
+
+
+
+class Subject(AbstractSubject):
     def __init__(self):
         self.div_obj = DivObserver(4)
         self.mod_obj = ModObserver(3)
@@ -26,9 +56,7 @@ class Subject(object):
         self.value = value
         self.notify()
 
-    def notify(self):
-        self.div_obj.update(self.value)
-        self.mod_obj.update(self.value)
+
 
 
 if __name__ == '__main__':
